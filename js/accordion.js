fbe3ecf23b0154b556b1dc01cc9f7325eedb4bc7
Vadim <luckjantsev@rambler.ru>
2022-03-29 20:36:59 +0300

    29032022

---
+++ js/accordion.js
@@ -0,0 +1,117 @@
+(() => {
+    new Accordion(".js-accordion-container", {
+      openOnInit: [0]
+    });
+  })();
+
+let hi1sost=0;
+let li1=document.querySelector(".accordion__li1")
+let hi1=document.getElementById("hicon1")
+li1.addEventListener("click",rotateli1)
+
+function rotateli1(){
+  if (hi1sost==0){  
+    hi1.style.transform="rotate(180deg)";
+    return hi1sost=1;
+      }
+      else{
+      hi1.style.transform="rotate(360deg)" 
+      return hi1sost=0;
+}
+}
+
+let hi2sost=0;
+let li2=document.querySelector(".accordion__li2")
+let hi2=document.getElementById("hicon2")
+li2.addEventListener("click",rotateli2)
+
+function rotateli2(){
+  if (hi2sost==0){  
+    hi2.style.transform="rotate(180deg)";
+    return hi2sost=1;
+      }
+      else{
+      hi2.style.transform="rotate(360deg)" 
+      return hi2sost=0;
+}
+}
+
+let hi3sost=0;
+let li3=document.querySelector(".accordion__li3")
+let hi3=document.getElementById("hicon3")
+li3.addEventListener("click",rotateli3)
+
+function rotateli3(){
+  if (hi3sost==0){  
+    hi3.style.transform="rotate(180deg)";
+    return hi3sost=1;
+      }
+      else{
+      hi3.style.transform="rotate(360deg)" 
+      return hi3sost=0;
+}
+}
+
+let hi4sost=0;
+let li4=document.querySelector(".accordion__li4")
+let hi4=document.getElementById("hicon4")
+li4.addEventListener("click",rotateli4)
+
+function rotateli4(){
+  if (hi4sost==0){  
+    hi4.style.transform="rotate(180deg)";
+    return hi4sost=1;
+      }
+      else{
+      hi4.style.transform="rotate(360deg)" 
+      return hi4sost=0;
+}
+}
+
+let hi5sost=0;
+let li5=document.querySelector(".accordion__li5")
+let hi5=document.getElementById("hicon5")
+li5.addEventListener("click",rotateli5)
+
+function rotateli5(){
+  if (hi5sost==0){  
+    hi5.style.transform="rotate(180deg)";
+    return hi5sost=1;
+      }
+      else{
+      hi5.style.transform="rotate(360deg)" 
+      return hi5sost=0;
+}
+}
+
+let hi6sost=0;
+let li6=document.querySelector(".accordion__li6")
+let hi6=document.getElementById("hicon6")
+li6.addEventListener("click",rotateli6)
+
+function rotateli6(){
+  if (hi6sost==0){  
+    hi6.style.transform="rotate(180deg)";
+    return hi6sost=1;
+      }
+      else{
+      hi6.style.transform="rotate(360deg)" 
+      return hi6sost=0;
+}
+}
+
+let hi7sost=0;
+let li7=document.querySelector(".accordion__li7")
+let hi7=document.getElementById("hicon7")
+li7.addEventListener("click",rotateli7)
+
+function rotateli7(){
+  if (hi7sost==0){  
+    hi7.style.transform="rotate(180deg)";
+    return hi7sost=1;
+      }
+      else{
+      hi7.style.transform="rotate(360deg)" 
+      return hi7sost=0;
+}
+}
