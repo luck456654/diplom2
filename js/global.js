91f0cc1463fe59c60b43b2ec5cff54a3dd694a60
Vadim <luckjantsev@rambler.ru>
2022-05-04 16:42:00 +0300

    04052022

--- js/global.js
+++ js/global.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-    $("#body").on("click","a", function (event) {
+$(document).ready(function () {
+    $("#body").on("click", "a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({ scrollTop: top }, 1500);
     });
 });
+tippy('#tooltip1', {
+    content: 'Пример современных тенденций - современная методология разработки!',
+});
+tippy('#tooltip2', {
+    content: 'Приятно, граждане, наблюдать, как сделанные на базе аналитики выводы вызывают у вас эмоции',
+});
+tippy('#tooltip3', {
+    content: 'В стремлении повысить качество',
+});
+
--- js/validationinit.js
+++ js/validationinit.js
@@ -5,31 +5,31 @@ validation
     {
       rule: 'minLength',
       value: 3,
-      errorMessage: 'Имя не может быть менее 3 символов',
+      errorMessage: 'Недопустимый формат',
     },
     {
       rule: 'maxLength',
       value: 30,
-      errorMessage: 'Имя не может быть боллее 30 символов',
+      errorMessage: 'Недопустимый формат',
     },
     {
         rule: 'required',
-        errorMessage: 'Поле "Имя" не может быть пустым',
+        errorMessage: 'Недопустимый формат',
       },
   ])
   .addField('#tel', [
     {
       rule: 'required',
-      errorMessage: 'Поле "Телефон" не может быть пустым',
+      errorMessage: 'Недопустимый формат',
     },
     {
         rule: 'minLength',
         value: 5,
-        errorMessage: 'Поле "Телефон" не может быть менее 5 символов',
+        errorMessage: 'Недопустимый формат',
       },
       {
         rule: 'maxLength',
         value: 11,
-        errorMessage: 'Поле "Телефон" не может быть боллее 11 символов',
+        errorMessage: 'Недопустимый формат',
       },
   ]);
